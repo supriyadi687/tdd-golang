a3ed35e17209868997e77260bf3b3df518d6f5a1
Agus Supriyadi <agus@durian.money>
2021-01-09 06:15:48 +0700

    02 divisible by 5 - implement

--- foobarqix/foobarqix.go
+++ foobarqix/foobarqix.go
@@ -1,5 +1,19 @@
 package foobarqix
 
+import "strconv"
+
 func Compute(input string) string {
-	return "Foo"
+	number, err := strconv.Atoi(input)
+	if err != nil {
+		return "unable to parse input"
+	}
+	if number % 3 == 0 {
+		return "Foo"
+	}
+
+	if number % 5 == 0 {
+		return "Bar"
+	}
+
+	return strconv.Itoa(number)
 }
